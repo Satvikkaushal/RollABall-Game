7008d0001fb1fc205674ae324fbc60c7dffd5583
satvik1002 <satvikkaushal8@gmail.com>
2020-02-10 20:06:29 +0530

    updated the project

---
+++ Game_416/Assets/Scripts/callbackfunc.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class callbackfunc : MonoBehaviour
+{
+        void OnEnable()
+        {
+            Debug.Log("To");
+        }
+
+        void Update()
+        {
+            Debug.Log("Rythmos");
+        }
+
+        void LateUpdate()
+        {
+            Debug.Log("Family");
+        }
+
+        void FixedUpdate()
+        {
+            Debug.Log("from");
+        }
+
+        void Start()
+        {
+            Debug.Log("Freshers");
+        }
+
+        void Awake()
+        {
+        Debug.Log("Welcome");
+        }
+    }
---
+++ Game_416/Assets/Scripts/level.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class level : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("level0"))
+        {
+            SceneManager.LoadScene("samplescene2");
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
